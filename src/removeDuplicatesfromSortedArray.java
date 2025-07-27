fa42bdf5eb38f73f888b14720f191667d749ffc8
mrsidhantkr <mrsidhantkr@gmail.com>
2025-07-27 20:02:52 +0530

    change in second code

--- src/removeDuplicatesfromSortedArray.java
+++ src/removeDuplicatesfromSortedArray.java
@@ -1,5 +1,23 @@
 public class removeDuplicatesfromSortedArray {
     public static void main(String[] args) {
-        
+        int[] nums = {1,2,3,44,44};
+        System.out.println(removeDuplicates(nums));
+
+    }
+
+
+    static int removeDuplicates(int[] nums){
+
+      int i = 0;
+
+      for(int  j =1;j<nums.length;j++){
+          if(nums[i]<nums[j]){
+              i++;
+
+              nums[i]=nums[j];
+          }
+      }
+
+      return i+1;
     }
 }
